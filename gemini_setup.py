5fd51fa45848bcea5e3ae557ca66e5bd267a284d
Tejaswini <abburutejashwini@gmail.com>
2025-08-13 11:57:30 +0530

    gemini_setup.py

--- gemini_setup.py
+++ gemini_setup.py
@@ -1,12 +1,13 @@
-temp/
+
 
 import google.generativeai as genai
 import os
 
-genai.configure(api_key="AIzaSyCXHqnLr4dpoCIuzfMMo40x5xU3oIDKR1M")
+genai.configure(api_key="")
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 response= model.generate_content("Hello Gemini!!")
 print(response.text)
+
 
 
