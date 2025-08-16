316152572c1ac3f86215c8357cc8e98192ac0b29
Tejaswini <abburutejashwini@gmail.com>
2025-08-16 18:48:48 +0530

    Update gemini_setup.py

--- gemini_setup.py
+++ gemini_setup.py
@@ -1,11 +1,4 @@
-import google.generativeai as genai
-import os
 
-genai.configure(api_key="")
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-response= model.generate_content("Hello Gemini!!")
-print(response.text)
 
 
 
